cfbfd17730b2ce6275c200a62854ea43fc3881f1
Snowspect <s155384@student.dtu.dk>
2018-10-30 13:53:15 +0100

    Added projet

---
+++ Ship/Assets/Scripts/MovementNotes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementNotes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		#region force related to rolling
+		/*	
+ 		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//rbd.AddForce (movement * speed);
+*/
+		#endregion
+
+	}
+}
