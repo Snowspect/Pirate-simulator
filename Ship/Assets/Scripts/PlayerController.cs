78a21bc46cd9c0fd5bf3b803e58efdc9de44585f
Snowspect <s155384@student.dtu.dk>
2018-11-02 19:02:05 +0100

    Player controller
    Commented code and updated variable references.

--- Ship/Assets/Scripts/PlayerController.cs
+++ Ship/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 0;
-	private int n = 0;
+	public float speed;
 	public float mass;
 	public float accelerationfactor;
-	public float speedLimit;
+	public float maximumSpeed;
+	public float minimumSpeed;
+	public float minimumSpeedTrigger;
 	public float slowfactor;
 	public float turnfactor;
+	public float speedbuff;
 	bool moving;
 	float moveHorizontal;
 	float moveVertical;
@@ -20,95 +22,91 @@ public class PlayerController : MonoBehaviour {
 	void Start () 
 	{
 		ship = GetComponent<Rigidbody> ();
-		moving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		/*
-		#region sets top speed and lowest speed
-
-		//Currently just stops the moment the fingers are taken off the up key or down key
-		//Adds/removes speed everytime the script updates
-		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
-		{
-			if(speed <= 2f)
-			{
-				speed = speed + 0.2f;
-			}
-		}
-		else 
-		{
-			if(speed > 0f)
-			{
-				speed = speed - 0.2f;
-			}
-		}
-		#endregion
-
-		#region movement
-		moveHorizontal = Input.GetAxis ("Horizontal")*Time.deltaTime*speed;
-		//moveVertical = Input.GetAxis ("Vertical")*Time.deltaTime*speed;
-
-	
-		//moveHorizontal = moveHorizontal * (-1); // reverts movement based on input (due to bug, goes left when pressing right, and vise versa)
-
-
-/*		Vector3 curve = new Vector3(moveVertical, 0.0f, moveHorizontal);
-		Vector3 changeCurve = new Vector3 (moveHorizontal,0.0f,moveVertical);
-		Vector3 newCurve = changeCurve + curve;
-
-
-		//Vector3 movement = new Vector3(transform.forward, 0.0f, moveHorizontal);
-		//ship.AddRelativeForce(Vector3.forward * speed);
-		transform.Translate(transform.forward * speed, Space.World); //With vector movements
-		//transform.localPosition = 
-
-		#endregion
-
-
-		#region Looking direction
-		//looks in moving direction and keeps direction after finished movement.
-
-		Vector3 lookDirection = new Vector3(moveHorizontal*(-1), 0.0f, moveVertical);
-		if(lookDirection != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), 0.9f);
-		#endregion 
-
-		//transform.Translate (moveVertical, 0.0f, moveHorizontal , Space.World); //without float movement
-	
-
-		#region tilts the ship akwardly, could possibly be used
-		/*		
- 		lookDirection += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-		transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, targetRotation, speed*Time.deltaTime);
-		#endregion
-
-		#region
-
-
-
-		#endregion
-		*/
-
-		n = 1;
-		moveCharacter ();
-		RotateCharacter ();
+		moveCharacter (); //implementation of movement
+		RotateCharacter (); //implementation of rotation
 	}
 
 	void moveCharacter()
 	{
+		//accelerate the ship as a factor of its acceleration factor/ mass.
+		//the acceleration factor can present for example the ships flags to make it easier to move forward.
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			if (moving == false) {
-				speed = speed + 0.001f;
-				moving = true;
-			}
-			if (speed <= speedLimit) {
+			if (speed <= maximumSpeed) {
 				float speedfactor = accelerationfactor / mass;
 				speed = speed + speedfactor;
+			}
+		}
+		// Down arrow impl.
+		/* else if (Input.GetKey(KeyCode.DownArrow)) {
+				if (speed > 0f) {
+					speed = speed - 0.2f;
+				} } */	
 
-				//An attempt on "gradual speed increase"
+		//neither up or down is pressed
+			else if (!Input.GetKey (KeyCode.UpArrow) /*&& !Input.GetKey (KeyCode.DownArrow)*/) {
+			if (speed > minimumSpeed) {
+				speed = speed * 0.99f;
+				//the below commented out code is an attempt at making mass and the slowfactor play a role, but it isn't properly converted to 0.99f. so not ready yet.
+				//the massremovalfactor defines the number we have to remove from the decrease factor as these two play a crucial rule together
+				//float massremovalfactor = mass/100;
+				//slowfactor = mass - massremovalfactor;
+				/*Debug.Log ("slow factor : " + slowfactor);
+				Debug.Log ("mass removal : " + massremovalfactor);
+				Debug.Log("slow factor times 0.1 : " + slowfactor * 0.1f);
+
+
+				float decreasefactor = slowfactor - speedbuff;
+				*/
+				//float decreasefactor = slowfactor / mass;
+			} 
+		}
+		//so the ship doesn't go into a n^e factor indefinietly.
+		if (speed < minimumSpeedTrigger) 
+			{
+				speed = minimumSpeed;
+			}
+		//moves the ship in relation to the world, taking the ships internal coordinate system into consideration (found through experimentation)
+		transform.Translate (transform.right * speed * Time.deltaTime, Space.World); //the coordinate system of the ship has switched horizontal with right and up so we do the same.
+	}
+
+	//allows the ship to curve in left or right direction
+	void RotateCharacter()
+	{
+		/*
+		 * TODO
+		 * turnfactor should be based on mass and a procentage factor
+		 * procentage factor should be based on buffs.
+		 * rotation needs to be based on speed!. ALSO, implement speedup in relation to mass in the above code. //display backwards.
+		 */
+
+		//curves in direction as long as speed isn't zero.
+		if(Input.GetKey(KeyCode.LeftArrow) && speed != minimumSpeed) 
+		{
+			//rotation factor dictactes how sharp the curve is.
+			float rotationfactor = (turnfactor * speed);
+			//-1*rotation factor is to make it turn left, if we remove it, it turns right.
+			//space.World as we want to move it accordingly to the coordinate system in the world.
+			transform.Rotate (transform.up, (-1*rotationfactor) * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && speed != minimumSpeed)
+		{
+			float rotationfactor = turnfactor * speed;
+			transform.Rotate (transform.up, rotationfactor * Time.deltaTime, Space.World);
+		}
+	}
+}
+
+
+
+//Not used, but could be helpful for other things.
+
+
+			//An attempt on "gradual speed increase"
 				/*if (speed < 2f)
 					speed = speed + 0.01f;
 					//speed = speed + 0.05f;
@@ -117,53 +115,17 @@ public class PlayerController : MonoBehaviour {
 					speed = speed + 0.2f;
 					speed = speed + speed + 0.2f; //can be used for burst skill
 				*/
-			}
-		}
-		// Down arrow impl.
-		/*
-			else if (Input.GetKey(KeyCode.DownArrow))
-			{
-				if (speed > 0f)
-				{
-					speed = speed - 0.2f;
-				}
-			}
 
-		*/	
-			else if (!Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.DownArrow)) {
-			if (speed > 0f) {
-				float decreasefactor = slowfactor / mass;
-				speed = speed * 0.99f;
-				//speed = speed - 0.01f; //slows down linearly
+			//speed = speed - 0.01f; //slows down linearly
 				/*if(speed >= 2f) 
 					speed = speed - 0.02f*speed;
 				if (speed < 2 && speed > 0f)
 					speed = speed - 0.04f * speed;
 				}*/
-			}
-		}
-		if (speed < 0.001f) 
-			{
-				speed = 0f;
-			}
-		transform.Translate (transform.right * speed * Time.deltaTime, Space.World); //the coordinate system of the ship has switched horizontal with right and up so we do the same.
-	}
 
-	void RotateCharacter()
-	{
-		/*
-		 * turnfactor should be based on mass and a procentage factor
-		 * procentage factor should be based on buffs.
-		 */
-		if(Input.GetKey(KeyCode.LeftArrow) && speed != 0) //rotation needs to be based on speed!. ALSO, implement speedup in relation to mass in the above code. //display backwards.
-		{
-			float rotationfactor = (turnfactor * speed);
-			transform.Rotate (transform.up, (-1*rotationfactor) * Time.deltaTime, Space.World);
-		}
-		if (Input.GetKey (KeyCode.RightArrow) && speed != 0)
-		{
-			float rotationfactor = turnfactor * speed;
-			transform.Rotate (transform.up, rotationfactor * Time.deltaTime, Space.World);
-		}
-	}
-}
+
+/*		#region til	ts the ship akwardly, could possibly be used	
+ 		lookDirection += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+		transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, targetRotation, speed*Time.deltaTime);
+		#endregion*/
