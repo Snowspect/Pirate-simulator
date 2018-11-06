c30e6b24ae4fc411cfc59ffd193975eceb3692b3
Snowspect <s155384@student.dtu.dk>
2018-11-06 13:26:08 +0100

    Ship, and dynamic ship control
    Added new ship model
    Changed playercontroller to adjust to ship tag (light, medium, heavy)
    Changed calculations of rotation, acceleration etc.

--- Ship/Assets/Scripts/PlayerController.cs
+++ Ship/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour {
 
@@ -13,22 +15,28 @@ public class PlayerController : MonoBehaviour {
 	public float slowfactor;
 	public float turnfactor;
 	public float speedbuff;
-	bool moving;
+	public float minimumRotationSpeed;
 	float moveHorizontal;
 	float moveVertical;
 	private Vector3 lookDirection = Vector3.zero;
 	Rigidbody ship;
+
+
 	// Use this for initialization
 	void Start () 
 	{
+		setShipStandard ();
+
 		ship = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		setShipStandard ();
 		moveCharacter (); //implementation of movement
 		RotateCharacter (); //implementation of rotation
+
 	}
 
 	void moveCharacter()
@@ -71,7 +79,11 @@ public class PlayerController : MonoBehaviour {
 				speed = minimumSpeed;
 			}
 		//moves the ship in relation to the world, taking the ships internal coordinate system into consideration (found through experimentation)
-		transform.Translate (transform.right * speed * Time.deltaTime, Space.World); //the coordinate system of the ship has switched horizontal with right and up so we do the same.
+		transform.Translate (transform.forward * speed * Time.deltaTime, Space.World); //the coordinate system of the ship has switched horizontal with right and up so we do the same.
+
+		lookDirection += new Vector3(0f,0f,0f);
+		Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+		transform.rotation = Quaternion.LerpUnclamped(transform.rotation, targetRotation, speed*Time.deltaTime);
 	}
 
 	//allows the ship to curve in left or right direction
@@ -85,18 +97,67 @@ public class PlayerController : MonoBehaviour {
 		 */
 
 		//curves in direction as long as speed isn't zero.
-		if(Input.GetKey(KeyCode.LeftArrow) && speed != minimumSpeed) 
+		if(Input.GetKey(KeyCode.LeftArrow) && speed != minimumSpeed && speed >= minimumRotationSpeed) 
 		{
 			//rotation factor dictactes how sharp the curve is.
-			float rotationfactor = (turnfactor * speed);
+			//float rotationfactor = (turnfactor - (speed));
+			float rotationfactor = turnfactor;
 			//-1*rotation factor is to make it turn left, if we remove it, it turns right.
 			//space.World as we want to move it accordingly to the coordinate system in the world.
 			transform.Rotate (transform.up, (-1*rotationfactor) * Time.deltaTime, Space.World);
 		}
-		if (Input.GetKey (KeyCode.RightArrow) && speed != minimumSpeed)
+		if (Input.GetKey (KeyCode.RightArrow) && speed != minimumSpeed && speed >= minimumRotationSpeed)
 		{
-			float rotationfactor = turnfactor * speed;
+//			float rotationfactor = (turnfactor - (speed));
+			float rotationfactor = (turnfactor / speed);
+//			float rotationfactor = turnfactor * speed;
 			transform.Rotate (transform.up, rotationfactor * Time.deltaTime, Space.World);
+		}
+
+	}
+
+
+
+	void setShipStandard()
+	{
+		if (this.tag.Equals("light"))
+		{
+			mass = 180f;
+			accelerationfactor = 1000f / mass; 
+			maximumSpeed = (1000f / mass); // 6,6
+			minimumSpeed = 0f;
+			//			minimumSpeedTrigger = 0.01f;
+			//			minimumRotationSpeed = 0.01f;
+			//slowfactor = 
+			//			speedbuff = 0f;
+			turnfactor = 7000f / mass;
+			RectTransform t = this.GetComponent<RectTransform> ();
+
+		}
+		if (this.tag.Equals("medium"))
+		{
+			mass = 250f;
+			accelerationfactor = 1000f / mass; 
+			maximumSpeed = (1000f / mass); // 6,6
+			minimumSpeed = 0f;
+			//			minimumSpeedTrigger = 0.01f;
+			//			minimumRotationSpeed = 0.01f;
+			//slowfactor = 
+			//			speedbuff = 0f;
+			turnfactor = 7000f / mass;
+
+		}
+		if (this.tag.Equals("heavy"))
+		{
+			mass = 300f;
+			accelerationfactor = 1000f / mass; 
+			maximumSpeed = (1000f / mass); // 6,6
+			minimumSpeed = 0f;
+			//			minimumSpeedTrigger = 0.01f;
+			//			minimumRotationSpeed = 0.01f;
+			//slowfactor = 
+			//			speedbuff = 0f;
+			turnfactor = 7000f / mass;
 		}
 	}
 }
@@ -105,7 +166,7 @@ public class PlayerController : MonoBehaviour {
 
 //Not used, but could be helpful for other things.
 
-
+/*
 			//An attempt on "gradual speed increase"
 				/*if (speed < 2f)
 					speed = speed + 0.01f;
@@ -115,7 +176,7 @@ public class PlayerController : MonoBehaviour {
 					speed = speed + 0.2f;
 					speed = speed + speed + 0.2f; //can be used for burst skill
 				*/
-
+/*
 			//speed = speed - 0.01f; //slows down linearly
 				/*if(speed >= 2f) 
 					speed = speed - 0.02f*speed;
@@ -127,5 +188,5 @@ public class PlayerController : MonoBehaviour {
 /*		#region til	ts the ship akwardly, could possibly be used	
  		lookDirection += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 		Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-		transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, targetRotation, speed*Time.deltaTime);
+		transform.rotation = Quaternion.lerpUnclamped(transform.rotation, targetRotation, speed*Time.deltaTime);
 		#endregion*/
