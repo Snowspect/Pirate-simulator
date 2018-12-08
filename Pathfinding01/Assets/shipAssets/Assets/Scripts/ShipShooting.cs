e929c86d1397ec56b563180c09c1e1f363b8afd1
Snowspect <s155384@student.dtu.dk>
2018-12-08 12:20:55 +0100

    cleaning code

--- Pathfinding01/Assets/shipAssets/Assets/Scripts/ShipShooting.cs
+++ Pathfinding01/Assets/shipAssets/Assets/Scripts/ShipShooting.cs
@@ -18,53 +18,52 @@ public class ShipShooting : MonoBehaviour {
 	private string m_FireButton;
 	private float m_CurrentLaunchForce;
 	//private float m_ChargeSpeed;
-	private bool m_Fired;
+	private bool m_Fired; 
 
-	private void OnEnable()
-	{
-		m_CurrentLaunchForce = m_MinLaunchForce; //so we reset the ship when it is remade
+	private void OnEnable() 
+	{ 
+		m_CurrentLaunchForce = m_MinLaunchForce; //so we reset the ship when it is remade 
 
-	}
+	} 
 
 	// Use this for initialization
 	void Start () 
-	{
-		m_FireButton = "Fire" + m_PlayerNumber;
-		canons = new List<Transform> (2);
-		canons2.Add(m_FireTransform_rightSide_back);
-		canons2.Add(m_FireTransform_rightSide_front);
-		canons2.Add(m_FireTransform_rightSide_mid);
-		//m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
-	}
-	
+	{ 
+		m_FireButton = "Fire" + m_PlayerNumber; 
+		canons = new List<Transform> (2); 
+		canons2.Add(m_FireTransform_rightSide_back); 
+		canons2.Add(m_FireTransform_rightSide_front); 
+		canons2.Add(m_FireTransform_rightSide_mid); 
+		//m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime; 
+	} 
+ 	
 	// Update is called once per frame
 	void Update () 
-	{
-		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
-			//at max charge, not fired
-			m_CurrentLaunchForce = m_MaxLaunchForce;
-			//Fire();
-			Fire2();
-		} else if (Input.GetButtonDown (m_FireButton)) {
-			//have we pressed the fire for the first time?
+	{ 
+		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) { //Gets triggered if we have max charge 
+			//at max charge, not fired //currently just firing due to cannons not needing to charge 
+			m_CurrentLaunchForce = m_MaxLaunchForce; 
+			//Fire2(); 
+			//recharge ();
+		} else if (Input.GetButtonDown (m_FireButton)) { //only gets triggered if we aren't pressing the button
+			//have we pressed the fire for the first time? 
 			m_Fired = false; 
-			m_CurrentLaunchForce = m_MinLaunchForce;
-		
-		} else if (Input.GetButton (m_FireButton) && !m_Fired) {
-			// holding button, not yet fired
-			m_CurrentLaunchForce += m_CurrentLaunchForce*Time.deltaTime;
+//			m_CurrentLaunchForce = m_MinLaunchForce; 
+		} else if (Input.GetButton (m_FireButton) && !m_Fired) { 
+			// holding button, not yet fired 
+			// m_CurrentLaunchForce += m_CurrentLaunchForce*Time.deltaTime; 
+		} 
+		else if (Input.GetButtonUp (m_FireButton) && !m_Fired) //Used 
+		{ 
+			//we released the button, having not fired yet 
+			//Fire(); 
+			Fire2 (); 
+		} 
+	} 
 
-		} else if (Input.GetButtonUp (m_FireButton) && !m_Fired) 
-		{
-			//we released the button, having not fired yet
-			//Fire();
-			//Fire2 ();
-		}
-	}
-
-	private void Fire()
-	{
-		m_Fired = true;
+	private void Fire() 
+	{ 
+		m_Fired = true; 
 		//instanciate a canonBall instance at the position and rotation of our fireTransform
 		Rigidbody canonBallInstance = Instantiate (m_cannonball, m_FireTransform_rightSide_back.position, m_FireTransform_rightSide_back.rotation) as Rigidbody;
 		canonBallInstance.velocity = m_CurrentLaunchForce * m_FireTransform_rightSide_back.forward;
@@ -86,4 +85,14 @@ public class ShipShooting : MonoBehaviour {
 
 		}
 	}
-}
+
+	private void recharge()
+	{
+		float counter = 0; 
+		while (counter <= 10) 
+		{ 
+			counter += Time.deltaTime;
+			Debug.Log (counter);
+		} 
+	} 
+} 
