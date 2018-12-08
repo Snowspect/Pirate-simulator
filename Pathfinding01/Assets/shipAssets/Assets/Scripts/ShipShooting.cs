e0be7302d118ee3951b3cd47a9e93b4c079ff147
Snowspect <s155384@student.dtu.dk>
2018-12-08 20:11:16 +0100

    Cannon functionality updated
    - Now able to shoot cannons on both sides
    - Now recharge time on cannons

--- Pathfinding01/Assets/shipAssets/Assets/Scripts/ShipShooting.cs
+++ Pathfinding01/Assets/shipAssets/Assets/Scripts/ShipShooting.cs
@@ -3,96 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipShooting : MonoBehaviour {
-
-	public float m_MinLaunchForce = 15f;
-	public float m_MaxLaunchForce = 30f;
-	public float m_MaxChargeTime = 0.75f;
-	public List<Transform> canons;
-	public List<Transform> canons2;
-	public Transform m_FireTransform_rightSide_back;
-	public Transform m_FireTransform_rightSide_mid;
-	public Transform m_FireTransform_rightSide_front;
-	public int m_PlayerNumber = 1;
-	public Rigidbody m_cannonball;
-
-	private string m_FireButton;
-	private float m_CurrentLaunchForce;
-	//private float m_ChargeSpeed;
-	private bool m_Fired; 
+	
+	public float m_MaxLaunchForce = 30f; 
+	public List<Transform> canonsLeft; 
+	public List<Transform> canonsRight;
+	public Rigidbody m_cannonball; 
+	private float rechargeLeftSideTime;
+	private float rechargeRightSideTime;
+	public string m_FireButton1;
+	public string m_FireButton2; 
+	public bool m_FiredLeft; 
+	public bool m_FiredRight; 
 
 	private void OnEnable() 
 	{ 
-		m_CurrentLaunchForce = m_MinLaunchForce; //so we reset the ship when it is remade 
-
 	} 
 
 	// Use this for initialization
 	void Start () 
 	{ 
-		m_FireButton = "Fire" + m_PlayerNumber; 
-		canons = new List<Transform> (2); 
-		canons2.Add(m_FireTransform_rightSide_back); 
-		canons2.Add(m_FireTransform_rightSide_front); 
-		canons2.Add(m_FireTransform_rightSide_mid); 
-		//m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime; 
+		rechargeLeftSideTime = 0f; 
+		rechargeRightSideTime = 0f; 
 	} 
  	
-	// Update is called once per frame
+	// Update is called once per frame 
 	void Update () 
 	{ 
-		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) { //Gets triggered if we have max charge 
-			//at max charge, not fired //currently just firing due to cannons not needing to charge 
-			m_CurrentLaunchForce = m_MaxLaunchForce; 
-			//Fire2(); 
-			//recharge ();
-		} else if (Input.GetButtonDown (m_FireButton)) { //only gets triggered if we aren't pressing the button
-			//have we pressed the fire for the first time? 
-			m_Fired = false; 
-//			m_CurrentLaunchForce = m_MinLaunchForce; 
-		} else if (Input.GetButton (m_FireButton) && !m_Fired) { 
-			// holding button, not yet fired 
-			// m_CurrentLaunchForce += m_CurrentLaunchForce*Time.deltaTime; 
-		} 
-		else if (Input.GetButtonUp (m_FireButton) && !m_Fired) //Used 
+		shoot (); 
+	} 
+		
+
+	/// <summary>
+	/// SHOOTING
+	/// </summary>
+	private void fireLeft() 
+	{ 
+		foreach (var canon in canonsLeft) 
 		{ 
-			//we released the button, having not fired yet 
-			//Fire(); 
-			Fire2 (); 
+			Rigidbody canonBallInstance = Instantiate (m_cannonball, canon.position, canon.rotation) as Rigidbody; 
+			canonBallInstance.velocity = m_MaxLaunchForce * canon.forward; 
 		} 
 	} 
-
-	private void Fire() 
-	{ 
-		m_Fired = true; 
-		//instanciate a canonBall instance at the position and rotation of our fireTransform
-		Rigidbody canonBallInstance = Instantiate (m_cannonball, m_FireTransform_rightSide_back.position, m_FireTransform_rightSide_back.rotation) as Rigidbody;
-		canonBallInstance.velocity = m_CurrentLaunchForce * m_FireTransform_rightSide_back.forward;
-		m_CurrentLaunchForce = m_MinLaunchForce;
-		Rigidbody canonBallInstance1 = Instantiate (m_cannonball, m_FireTransform_rightSide_mid.position, m_FireTransform_rightSide_mid.rotation) as Rigidbody;
-		canonBallInstance1.velocity = m_CurrentLaunchForce * m_FireTransform_rightSide_mid.forward;
-		m_CurrentLaunchForce = m_MinLaunchForce;
-		Rigidbody canonBallInstance2 = Instantiate (m_cannonball, m_FireTransform_rightSide_front.position, m_FireTransform_rightSide_front.rotation) as Rigidbody;
-		canonBallInstance2.velocity = m_CurrentLaunchForce * m_FireTransform_rightSide_front.forward;
-		m_CurrentLaunchForce = m_MinLaunchForce;
-	}
-	private void Fire2()
+	private void fireRight()
 	{
-		m_Fired = true;
-		foreach (var canon in canons2) {
-			Rigidbody canonBallInstance = Instantiate (m_cannonball, canon.position, canon.rotation) as Rigidbody;
-			canonBallInstance.velocity = m_CurrentLaunchForce * canon.forward;
-			m_CurrentLaunchForce = m_MinLaunchForce;
-
+		foreach (var canon in canonsRight) 
+		{ 
+			Rigidbody canonBallInstance = Instantiate (m_cannonball, canon.position, canon.rotation) as Rigidbody; 
+			canonBallInstance.velocity = m_MaxLaunchForce * canon.forward; 
 		}
 	}
 
-	private void recharge()
-	{
-		float counter = 0; 
-		while (counter <= 10) 
-		{ 
-			counter += Time.deltaTime;
-			Debug.Log (counter);
+
+	/// <summary>
+	/// SHOOTING AND RECHARGING TRIGGER 
+	/// </summary>
+	private void shoot() 
+	{ 
+		if (rechargeLeftSideTime <= 0) 
+		{ //that is, if the recharging is done, make sure we can fire again 
+			m_FiredLeft = false; 
 		} 
+		if (rechargeRightSideTime <= 0) 
+		{
+			m_FiredRight = false;
+		}
+		if (m_FiredLeft == false && Input.GetButton (m_FireButton1)) //Ctlr 
+		{
+			fireLeft ();
+			rechargeLeftSideTime = 3; 
+			m_FiredLeft = true;
+		}
+		if (m_FiredRight == false && Input.GetButton (m_FireButton2)) //Alt
+		{ // that is, if recharge is done and the above if statement is executed
+			fireRight();
+			rechargeRightSideTime = 3; 
+			m_FiredRight = true; 
+		} 
+		else if (m_FiredLeft == true) 
+		{ //if we fired the button, begin the recharging process
+			rechargeLeftSide (); 
+		} else if (m_FiredRight == true) 
+		{ 
+			rechargeRightSide ();	
+		} 
+	} 
+
+	/// <summary>
+	/// RECHARGING
+	/// </summary>
+	private void rechargeLeftSide() 
+	{ 
+		rechargeLeftSideTime -= Time.deltaTime; 
+		Debug.Log ("Time until recharging is over : " + rechargeLeftSideTime); 
+	} 
+	private void rechargeRightSide()
+	{ 
+		rechargeRightSideTime -= Time.deltaTime; 
+		Debug.Log ("Time until recharging is over : " + rechargeRightSideTime); 
 	} 
 } 
---
+++ Pathfinding01/Assets/shipAssets/Assets/Scripts/ShootLeft.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RechargeLeft : MonoBehaviour {
+
+
+	public float m_MaxLaunchForce = 30f; 
+	public List<Transform> canonsLeft; 
+	public Rigidbody m_cannonball; 
+	private float rechargeLeftSideTime; 
+	public string m_FireButton1; 
+	public bool m_FiredLeft; 
+
+	// Use this for initialization
+	void Start () {
+		rechargeLeftSideTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		Trigger (); 
+	} 
+
+	//TRIGGERING LEFT SIDE
+	private void Trigger() 
+	{ 
+		if (rechargeLeftSideTime <= 0) 
+		{ //that is, if the recharging is done, make sure we can fire again 
+			m_FiredLeft = false; 
+		} 
+
+/*		if (rechargeRightSideTime <= 0) 
+		{ 
+			m_FiredRight = false;
+		} 
+*/
+		if (m_FiredLeft == false && Input.GetButton (m_FireButton1)) //The cannonballs haven't been fired and you press down "Ctrl" (To edit what button, either use functionality code or go into edit -> project settings -> input 
+		{
+			fireLeft ();
+			rechargeLeftSideTime = 3; 
+			m_FiredLeft = true;
+		}
+
+/*		if (m_FiredRight == false && Input.GetButton (m_FireButton2)) //Alt
+		{ // that is, if recharge is done and the above if statement is executed
+			fireRight();
+			rechargeRightSideTime = 3; 
+			m_FiredRight = true; 
+		} 
+*/
+		else if (m_FiredLeft == true) 
+		{ //if we fired the button, begin the recharging process
+			rechargeLeftSide (); 
+		} 
+
+/*		else if (m_FiredRight == true) 
+		{ 
+			rechargeRightSide ();	
+		} 
+*/
+	} 
+
+	/// <summary>
+	/// SHOOTING LEFT SIDE
+	/// </summary>
+	private void fireLeft() 
+	{ 
+		foreach (var canon in canonsLeft) 
+		{ 
+			Rigidbody canonBallInstance = Instantiate (m_cannonball, canon.position, canon.rotation) as Rigidbody; 
+			canonBallInstance.velocity = m_MaxLaunchForce * canon.forward; 
+		} 
+	} 
+
+	/// <summary>
+	/// RECHARGING LEFT SIDE
+	/// </summary>
+	private void rechargeLeftSide() 
+	{ 
+		rechargeLeftSideTime -= Time.deltaTime; 
+		Debug.Log ("Time until recharging is over : " + rechargeLeftSideTime); 
+	}  
+} 
