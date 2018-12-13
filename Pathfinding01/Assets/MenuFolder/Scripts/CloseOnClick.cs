4f0b09e51ca143c7996070468ce78b909c3053ce
mrtheide <jonasetn@hotmail.com>
2018-12-13 17:44:03 +0100

    Main Menu

---
+++ Pathfinding01/Assets/MenuFolder/Scripts/CloseOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOnClick : MonoBehaviour {
+
+	public void CloseProgram()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Pathfinding01/Assets/MenuFolder/Scripts/MenuShipData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShipData : MonoBehaviour {
+
+    public GameObject lightShip,mediumShip,heavyShip;
+    private void Update()
+    {
+        string ship = UserData.shipType;
+        if(ship == "Light" || true)
+        {
+            lightShip.SetActive(true);
+            mediumShip.SetActive(false);
+            heavyShip.SetActive(false);
+            lightShip.transform.Rotate(Vector3.up * Time.deltaTime*20);
+        }
+        else if(ship == "Medium")
+        {
+            lightShip.SetActive(false);
+            mediumShip.SetActive(true);
+            heavyShip.SetActive(false);
+            mediumShip.transform.Rotate(Vector3.up * Time.deltaTime * 20);
+        }
+        else if(ship == "Heavy")
+        {
+            lightShip.SetActive(false);
+            mediumShip.SetActive(false);
+            heavyShip.SetActive(true);
+            heavyShip.transform.Rotate(Vector3.up * Time.deltaTime * 20);
+        }
+    }
+    public void SetShipType( string type)
+    {
+        UserData.shipType = type;
+//        Shipdata.shiptype = type;
+    }
+}
